28ccb9b7963b0222c344a9e7ee7378f2178037dc
Erin McAtee <mcateee@gmail.com>
2016-01-21 00:02:31 -0600

    Updates to header & navigation. Set inner width for site at 1024 in _settings.

--- wp-content/themes/JointsWP-master/parts/nav-offcanvas-topbar.php
+++ wp-content/themes/JointsWP-master/parts/nav-offcanvas-topbar.php
@@ -1,19 +1,32 @@
 <!-- By default, this menu will use off-canvas for small
 	 and a topbar for medium-up -->
 
-<div class="top-bar" id="top-bar-menu">
-	<div class="top-bar-left float-left">
-		<ul class="menu">
-			<li class="menu-text"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
-		</ul>
+<div class="top-bar show-for-medium" id="top-bar-menu">
+	<div class="row">
+		<div class="top-bar-right float-right">
+			top bar menu stuff
+		</div>
 	</div>
-	<div class="top-bar-right show-for-medium">
-		<?php joints_top_nav(); ?>	
+</div>
+<div class="row" id="logo-bar">
+	<div class="logo-row row">
+		<div class="top-bar-left float-left">
+			<ul class="menu">
+				<li class="menu-text"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
+			</ul>
+		</div>
+		<div class="top-bar-right float-right show-for-small-only">
+			<ul class="menu">
+				<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
+				<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
+			</ul>
+		</div>
 	</div>
-	<div class="top-bar-right float-right show-for-small-only">
-		<ul class="menu">
-			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
-			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
-		</ul>
-	</div>
+</div>
+<div id="nav-bar" class="show-for-medium">
+	<div class="row">
+		<div class="top-bar-left row float-left">
+			<?php joints_top_nav(); ?>	
+		</div>
+	</div>	
 </div>
