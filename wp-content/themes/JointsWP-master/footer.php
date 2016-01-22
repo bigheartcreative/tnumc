eaba2d4f50ea297e7b10e174ae5bf3b0a84a845f
Adam Wheatley <wheatley.adam@me.com>
2016-01-22 14:42:07 -0600

    Update footer structure including social media icons

--- wp-content/themes/JointsWP-master/footer.php
+++ wp-content/themes/JointsWP-master/footer.php
@@ -1,9 +1,28 @@
 					<footer class="footer" role="contentinfo">
 						<div id="inner-footer" class="row">
-							<div class="large-12 medium-12 columns">
-								<nav role="navigation">
-		    						<?php joints_footer_links(); ?>
-		    					</nav>
+							<div class="small-9 medium-4 columns">
+								<h6>Contact Us</h6>
+								<p>304 S. Perimeter Park Drive</p>
+								<p>Nashville, TN 37211</p>
+								<p>615.329.1177</p>
+								<p><a href="#">info@TNUMC.org</a></p>
+								<p><a href="#">Hours &amp; Directions</a></p>
+								<p><a href="#">Privacy Policy</a></p>
+		    				</div>
+		    				<!-- replace with Forms widget -->
+		    				<div class="small-9 medium-4 columns">
+								<form action="">
+									<input type="text" placeholder="First Name">
+									<input type="text" placeholder="Last Name">
+									<input type="text" placeholder="Email">
+									<button type="submit">Submit</button>
+								</form>
+		    				</div>
+		    				<div class="small-9 medium-4 columns">
+								<h6>Connect With Us</h6>
+								<a href="#"><img src="/tnumc/wp-content/themes/JointsWP-master/assets/images/facebook.png" alt="Facebook"></a>
+								<a href="#"><img src="/tnumc/wp-content/themes/JointsWP-master/assets/images/twitter.png" alt="Twitter"></a>
+								<a href="#"><img src="/tnumc/wp-content/themes/JointsWP-master/assets/images/youtube.png" alt="YouTube"></a>
 		    				</div>
 							<div class="large-12 medium-12 columns">
 								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
