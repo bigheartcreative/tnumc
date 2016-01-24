61907021296f15843d08364d288b903fccead1f2
Erin McAtee <mcateee@gmail.com>
2016-01-24 17:24:37 -0600

    add top bar structure html & styles. create home page template.

--- wp-content/themes/JointsWP-master/parts/nav-offcanvas-topbar.php
+++ wp-content/themes/JointsWP-master/parts/nav-offcanvas-topbar.php
@@ -4,16 +4,21 @@
 <div class="top-bar show-for-medium" id="top-bar-menu">
 	<div class="row">
 		<div class="top-bar-right float-right">
-			top bar menu stuff
+			<a class="search-icon float-right"></a>
+			<a class="language float-right">English</a></li>
+			<a class="float-right">Staff</a>
+			<a class="float-right">Districts</a>
+			<a class="float-right">Bishop</a>
+			<a class="float-right">Churches</a>
 		</div>
 	</div>
 </div>
 <div class="logo-bar" id="logo-bar-menu">
 	<div class="logo-row row align-middle">
-		<div class="large-4 columns">
+		<div class="medium-4 columns">
 			<a class="logo" href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri();?>/assets/images/umclogo.png" alt="<?php bloginfo('name'); ?>" /></a>
 		</div>
-		<div class="large-8 columns text-right button-block">
+		<div class="medium-8 columns text-right button-block">
 			<a class="light-blue button" href="#">About Us</a>
 			<a class="light-blue button" href="#">Communications</a>
 			<a class="light-blue button" href="#">Forms</a>
