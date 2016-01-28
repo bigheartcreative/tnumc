f4616b2fd2daf6aa68d5da5adc5c2542d3a5e652
Adam Wheatley <wheatley.adam@me.com>
2016-01-27 22:26:07 -0600

    Edit an style article page header and byline

--- wp-content/themes/JointsWP-master/parts/content-byline.php
+++ wp-content/themes/JointsWP-master/parts/content-byline.php
@@ -1,3 +1,7 @@
-<p class="byline">
-	Posted on <?php the_time('F j, Y') ?> by <?php the_author_posts_link(); ?>  - <?php the_category(', ') ?>
+<p class="byline author">
+	By <?php the_author_posts_link(); ?>
 </p>	
+
+<p class="byline post_info">
+	<?php the_time('F j, Y') ?> | Nashville, TN
+</p>
