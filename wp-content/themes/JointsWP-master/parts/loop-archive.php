a22ea21839d81a1a179f85a89ff238901bed2c06
Erin McAtee <mcateee@gmail.com>
2016-04-19 20:59:40 -0500

    remove date & city from category list. Remove tags and replace with categories

--- wp-content/themes/JointsWP-master/parts/loop-archive.php
+++ wp-content/themes/JointsWP-master/parts/loop-archive.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">					
 	<header class="article-header">
 		<h2 class="search-result"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-		<?php get_template_part( 'parts/content', 'byline' ); ?>
+		<?php // get_template_part( 'parts/content', 'byline' ); ?>
 	</header> <!-- end article header -->
 					
 	<section class="entry-content" itemprop="articleBody">
@@ -10,7 +10,12 @@
 		
 	</section> <!-- end article section -->
 						
-	<footer class="article-footer">
-    	<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'jointstheme') . '</span> ', ', ', ''); ?></p>
+	<footer class="article-footer row">
+		<!-- This will be for both categories and tags -->
+		<div class="medium-9 columns">
+			<p class="tags">
+				<span class="tags-title"><?php _e( 'CATEGORIES: ', 'jointswp' ); ?></span><?php the_category(', '); ?>
+			</p>
+		</div>
 	</footer> <!-- end article footer -->				    						
 </article> <!-- end article -->
